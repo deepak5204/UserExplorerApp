ed778544e0584dc819eadb472f4142ee105795a0
deepak <pdeepak1255@gmail.com>
2026-06-28 17:37:15 +0530

    feat: display user data in Compose LazyColumn using Material3 card

--- app/src/main/java/com/example/userexplorerapp/MainActivity.kt
+++ app/src/main/java/com/example/userexplorerapp/MainActivity.kt
@@ -1,26 +1,39 @@
 package com.example.userexplorerapp
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.userexplorerapp.network.RetrofitInstance
 import com.example.userexplorerapp.ui.theme.UserExplorerAppTheme
 import com.example.userexplorerapp.viewmodel.UserViewModel
-import kotlinx.coroutines.launch
-import java.lang.Exception
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,9 +42,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             val viewModel: UserViewModel = viewModel() // bcz viewModel() is a composable function
             UserExplorerAppTheme {
-                    UserScreen(
-                        viewModel = viewModel
-                    )
+                UserScreen(
+                    viewModel = viewModel
+                )
             }
         }
     }
@@ -40,10 +53,98 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UserScreen(viewModel: UserViewModel) {
     val uiState by viewModel.uiState.collectAsState()
-    Text("Users size: ${uiState.users.size}")
+    val context = LocalContext.current
 
+    if (uiState.isLoading) {
+        CircularProgressIndicator()
+    } else if (uiState.error != null) {
+        Toast.makeText(context, uiState.error, Toast.LENGTH_SHORT).show()
+    } else {
+        val userList = uiState.users
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(userList.size) { index ->
+                val user = userList[index];
+
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp),
+                    elevation = CardDefaults.cardElevation(6.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Icon(imageVector = Icons.Default.Person, contentDescription = "profile")
+                            Column {
+                                Text(text = user.name)
+                                Text(text = user.username)
+                            }
+                        }
+
+
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Email, contentDescription = "email"
+                            )
+                            Text(text = user.email)
+                        }
+
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Phone, contentDescription = "phone"
+                            )
+                            Text(text = user.phone)
+                        }
+
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Build, contentDescription = "websites"
+                            )
+                            Text(text = user.website)
+                        }
+
+                        Column {
+                            Row(
+                                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.LocationOn,
+                                    contentDescription = "Location"
+                                )
+
+                                Column {
+                                    Text(text = user.address.city)
+                                    Row(
+                                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                                    ) {
+                                        Text(user.address.street)
+                                        Text(text = user.address.suite)
+                                    }
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/userexplorerapp/model/UserDto.kt
+++ app/src/main/java/com/example/userexplorerapp/model/UserDto.kt
@@ -2,8 +2,30 @@ package com.example.userexplorerapp.model
 
 data class UserDto(
     val id: Int,
+    val name: String,
+    val username: String,
     val email: String,
-    val firstName: String,
-    val lastName: String,
-    val avatar: String
+    val address: AddressDto,
+    val phone: String,
+    val website: String,
+    val company: CompanyDto
+)
+
+data class AddressDto(
+    val street: String,
+    val suite: String,
+    val city: String,
+    val zipcode: String,
+    val geo: GeoDto
+)
+
+data class GeoDto(
+    val lat: String,
+    val lng: String
+)
+
+data class CompanyDto(
+    val name: String,
+    val catchPhrase: String,
+    val bs: String
 )
